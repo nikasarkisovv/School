b06e63c6054af3fc3c2a03c6356f37351a996aff
Nika <Nika@DESKTOP-PFHESS0>
2024-01-13 21:34:52 +0200

    --

---
+++ Exercise/Temperature/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int max = int.MinValue, min = int.MaxValue;
+Random rnd = new Random();
+for (int i = 0; i < 30; i++)
+{
+    int temp = rnd.Next(-10, 45);
+    if (max < temp)
+    {
+        max = temp;
+    }
+    if (min > temp)
+    {
+        min = temp;
+    }
+}
+Console.WriteLine($"the temprature of the coldest day was {min} and the temprature of the hottest day was {max}");
