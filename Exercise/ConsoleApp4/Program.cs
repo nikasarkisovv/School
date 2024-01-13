d38d77944fd2c380b1dd7677a7c520bbbfdf3218
Nika <Nika@DESKTOP-PFHESS0>
2024-01-13 20:24:37 +0200

    ---

---
+++ Exercise/ConsoleApp4/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Please enter a number between 1 to 9");
+string input = Console.ReadLine();
+
+int num = int.Parse(input);
+
+for (int y = 1; y <= num; y++)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int y = 1; y <= num; y++)
+{
+    for (int i = num; i >= 1; i--)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int y = 1; y <= num; y++)
+{
+    for (int i = 1; i <= y; i++)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int y = 1; y <= num; y++)
+{
+    for (int i = y; i >= 1; i--)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int y = 1; y <= num; y++)
+{
+    if (y % 2 == 1)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.Write(i + " ");
+        }
+    }
+    else
+    {
+        for (int i = num; i >= 1; i--)
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    Console.WriteLine();
+}
---
+++ Exercise/FindPrimeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter a number:");
+string input = Console.ReadLine();
+int num = int.Parse(input);
+
+int tempNum = num / 2;
+int count = 0;
+while (tempNum > 1)
+{
+    count++;
+    if (num % tempNum == 0)
+    {
+        Console.WriteLine("The given number is not a prime number.");
+        return;
+    }
+    tempNum--;
+}
+
+Console.WriteLine("The given number is a Prime number");
+Console.WriteLine($"The program made {count} iterations of testing");
+
